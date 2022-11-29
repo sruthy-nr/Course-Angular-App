a9014de995a62eda9b582ddf4d2c45a475f8c8df
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 13:00:24 +0530

    service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  fetchCourse=()=>
+  {
+    return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
+  }
 }
